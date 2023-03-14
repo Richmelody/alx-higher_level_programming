4a95d2e6e1c27f24504a14899db209ae60e55ae1
Richmelody <chigs1million@gmail.com>
2023-03-14 11:11:27 -0700

    java script

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fun = 'C is fun' ;
-const cool = 'Python is cool' ;
-const amazing = 'JavaScript is amazing' ;
+const fun = 'C is fun';
+const cool = 'Python is cool';
+const amazing = 'JavaScript is amazing';
 
 console.log(fun + '\n' + cool + '\n' + amazing);
